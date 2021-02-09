b2fb4bbb72e47bbe4dc6787422801afe23190b67
Sergey <artanoir@gmail.com>
2021-02-09 16:55:26 +0300

    add new files

---
+++ DataTypes/src/main/java/main.java
@@ -0,0 +1,44 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("\nExample 1: Java boolean data type");
+        boolean dot = true;
+        System.out.println(dot);    // prints true
+
+        System.out.println("\nExample 2: Java byte data type");
+        byte range;
+        range = 124;
+        System.out.println(range);    // prints 124
+
+        System.out.println("\nExample 3: Java short data type");
+        short temperature;
+        temperature = -200;
+        System.out.println(temperature);  // prints -200
+
+        System.out.println("\nExample 4: Java int data type");
+        int row = -4250000;
+        System.out.println(row);  // print -4250000
+
+        System.out.println("\nExample 5: Java long data type");
+        long line = -42332200000L;
+        System.out.println(line);    // prints -42332200000
+
+        System.out.println("\nExample 6: Java double data type");
+        double number = -42.3;
+        System.out.println(number);  // prints -42.3
+
+        System.out.println("\nExample 7: Java float data type");
+        float stitch = -42.3f;
+        System.out.println(stitch);  // prints -42.3
+
+        System.out.println("\nExample 8: Java char data type");
+        char letter = '\u0051';
+        System.out.println(letter);  // prints Q
+
+        //- Here is another example:
+        System.out.println("\nHere is another example:");
+        char letter1 = '9';
+        System.out.println(letter1);  // prints 9
+        char letter2 = 65;
+        System.out.println(letter2);  // prints A
+    }
+}
---
+++ HomeWork/src/main/java/main.java
@@ -0,0 +1,70 @@
+public class main {
+    public static void main(String[] args) {
+        QuestionTwo();
+        System.out.println("a * (b + (c / d)) = " + QuestionThree(4,3,2,1));
+        System.out.println(QuestionFour(9, 2));
+        QuestionFive(-10);
+        System.out.println(QuestionSix(-5));
+        QuestionSeven("BANANA");
+        QuestionEight(2021);
+
+
+    }
+
+    static void QuestionTwo() {
+        System.out.println("2--------------");
+        boolean b = false;
+        byte bt = 3;
+        long l = 5L;
+        int i = 4;
+        short s = 8;
+        float f = 2.7F;
+        double d = 8.5;
+        char ch = 102; // символ 'f'
+        System.out.println("boolean = " + b);
+        System.out.println("byte = " + bt);
+        System.out.println("long = " + l);
+        System.out.println("int = " + i);
+        System.out.println("short = " + s);
+        System.out.println("float = " + f);
+        System.out.println("double = " + d);
+        System.out.println("char = " + ch);
+
+    }
+
+    static float QuestionThree(float a, float b, float c, float d) {
+        System.out.println("3--------------");
+        return a * (b + (c / d));
+    }
+
+    static boolean QuestionFour(int a, int b) {
+        System.out.println("4--------------");
+        int sum = a + b;
+        if (sum > 10 && sum < 20) return true;
+        else return false;
+
+    }
+
+    static void QuestionFive(int a) {
+        System.out.println("5--------------");
+        if (a >= 0) System.out.println("Число положительное: " + a);
+        else System.out.println("Число отрицательное: " + a);
+    }
+
+    static boolean QuestionSix(int a) {
+        System.out.println("6--------------");
+        if (a < 0) return true;
+        return false;
+    }
+
+    static void QuestionSeven(String name) {
+        System.out.println("7--------------");
+        System.out.println("GOODBY, " + name + "!:D");
+    }
+
+    static void QuestionEight(int year) {
+        System.out.println("*--------------");
+        if (!(year % 4 == 0) || ((year % 100 == 0) && !(year % 400 == 0))) System.out.println(year + " Не високосный");
+        else System.out.println(year + " Високосный");
+    }
+}
