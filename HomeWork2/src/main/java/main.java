98cbae2e2e344fe09a894b2546361cdd056d784c
Sergey <artanoir@gmail.com>
2021-02-11 00:01:23 +0300

    Question 4 for lesson 2

---
+++ HomeWork2/src/main/java/main.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main (String[] args) {
+        int[][] arr4 = new int[5][5];
+        for (int i = 0; i < arr4.length; i++) {
+            for (int j = 0, j2 = arr4[i].length; j < arr4[i].length; j++, j2--) {
+                if (i == j || i == (j2 - 1)) arr4[i][j] = 1;
+                System.out.print(arr4[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+}
