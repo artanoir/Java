efb1fe0dbc1161e5159f411e3e059ae554e779de
Sergey <artanoir@gmail.com>
2021-02-11 11:18:47 +0300

    Add 1,2,3 Questions

--- HomeWork2/src/main/java/main.java
+++ HomeWork2/src/main/java/main.java
@@ -1,13 +1,34 @@
 public class main {
     public static void main (String[] args) {
-        int[][] arr4 = new int[5][5];
-        for (int i = 0; i < arr4.length; i++) {
-            for (int j = 0, j2 = arr4[i].length; j < arr4[i].length; j++, j2--) {
-                if (i == j || i == (j2 - 1)) arr4[i][j] = 1;
-                System.out.print(arr4[i][j] + " ");
+
+        int[] arrOne = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        for (int i = 0; i < arrOne.length; i++) {
+            arrOne[i] = arrOne[i] == 1 ? 0 : 1;
+            System.out.print(arrOne[i] + " ");
+        }
+
+        System.out.print("\n");
+        int[] arrTwo = new int[8];
+        for (int i = 1, j = 0; i < arrTwo.length; i++) arrTwo[i] = j += 3;
+        for (int x : arrTwo) System.out.print(x + " ");
+
+        System.out.print("\n");
+        int[] arrThree = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < arrThree.length; i++) {
+            if (arrThree[i] < 6) arrThree[i] *= 2;
+            System.out.print(arrThree[i] + " ");
+        }
+
+        System.out.print("\n");
+        int[][] arrFour = new int[5][5];
+        for (int i = 0; i < arrFour.length; i++) {
+            for (int j = 0, j2 = arrFour[i].length; j < arrFour[i].length; j++, j2--) {
+                if (i == j || i == (j2 - 1)) arrFour[i][j] = 1;
+                System.out.print(arrFour[i][j] + " ");
             }
             System.out.print("\n");
         }
+
     }
 
 }
