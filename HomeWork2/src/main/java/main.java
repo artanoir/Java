2d20f234efe84378d262d2ce643b8b608a0c0043
Sergey <artanoir@gmail.com>
2021-02-12 21:47:26 +0300

    Update Home Work, beta version

--- HomeWork2/src/main/java/main.java
+++ HomeWork2/src/main/java/main.java
@@ -1,15 +1,15 @@
 public class main {
     public static void main (String[] args) {
 
-        int[] arrOne = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-
-
-
-
-
-
-
-
+        int[] one = {1, 1, 0, 0, 0, 1, 1, 1, 0, 1};
+        for (int i = 0; i < one.length; i++) {
+            if (one[i] == 1) {
+                one[i] = 0;
+            } else {
+                one[i] = 1;
+            }
+            System.out.print(one[i] + " ");
+        }
 
         System.out.print("\n");
         int[] arrTwo = new int[8];
@@ -33,6 +33,65 @@ public class main {
             System.out.print("\n");
         }
 
+        System.out.println("\r\n");
+        int[] arrFive = {10, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        int min = arrFive[0], max = arrFive[0], indexMax = 0, indexMin = 0;
+        for (int i = 0; i < arrFive.length; i++) {
+            if (arrFive[i] > max) {
+                max = arrFive[i];
+                indexMax = i;
+            }
+            if (arrFive[i] < min) {
+                min = arrFive[i];
+                indexMin = i;
+            }
+            System.out.println("Max i " + indexMax + " value  = " + max);
+            System.out.println("Мin i " + indexMin + " value  = " + min);
+        }
+
+        int[] arrSix = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        System.out.println("\r\nQuestion Six = " + checkBalance(arrSix));
+        System.out.println("\r\n shift +3"); shift(arrSix, +3);
+        System.out.println("\r\n shift -3"); shift(arrSix, -3);
+
+    }
+
+    private static boolean checkBalance(int[] arrParameter) {
+        int leftSum, rightSum;
+        for (int i = 0; i < arrParameter.length + 1; i++) {
+            leftSum = 0;
+            rightSum = 0;
+            for (int j = 0; j < i; j++) {
+                leftSum += arrParameter[j];
+            }
+            for (int j = i; j < arrParameter.length; j++) {
+                rightSum += arrParameter[j];
+            }
+            if (leftSum == rightSum) return true;
+        }
+        return false;
+    }
+    private static void shift(int[] arrPref, int n) {
+        for (int i : arrPref) System.out.print(i + " ");
+        System.out.println("\r\n");
+        if (n > 0) {
+            for (int x = 0; x < n; x++) {
+                int addShift = arrPref[arrPref.length - 1];
+                if (arrPref.length - 1 >= 0) System.arraycopy(arrPref, 0, arrPref, 1, arrPref.length - 1);
+                arrPref[0] = addShift;
+                for (int i : arrPref) System.out.print(i + " ");
+                System.out.println();
+            }
+        }
+        if (n < 0) {
+            for (int x = 0; x > n; x--) {
+                int addShift = arrPref[0];
+                System.arraycopy(arrPref, 1, arrPref, 0, arrPref.length - 1);
+                arrPref[arrPref.length - 1] = addShift;
+                for (int i : arrPref) System.out.print(i + " ");
+                System.out.print("\r\n");
+            }
+        }
     }
 
 }
