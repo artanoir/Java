bb2362c3260df4f912b88f7f0586f2d8c54d0a92
Sergey <artanoir@gmail.com>
2021-03-06 18:33:16 +0300

    update

--- Snake/src/main/java/Game.java
+++ Snake/src/main/java/Game.java
@@ -1,7 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.Random;
